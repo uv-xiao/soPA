4e50eba9e300fae48e97332a2dec39a6c36d487d
outerform <outerform@163.com>
2021-11-27 22:52:44 +0800

    new testcase virtualinvoke

---
+++ code/test/VirtualInvoke.java
@@ -0,0 +1,38 @@
+package test;
+
+import benchmark.internal.BenchmarkN;
+import benchmark.objects.A;
+import benchmark.objects.B;
+import benchmark.objects.H;
+
+/*
+ * @testcase FieldSensitivity2
+ *
+ * @version 1.0
+ *
+ * @author Johannes Späth, Nguyen Quang Do Lisa (Secure Software Engineering Group, Fraunhofer
+ * Institute SIT)
+ *
+ * @description Field Sensitivity without static method
+ */
+public class VirtualInvoke {
+
+  A x;
+
+  public VirtualInvoke() {
+    x=new A();
+  }
+
+
+
+  public static void main(String[] args) {
+
+    BenchmarkN.alloc(1);
+    A a=new A();
+    H h=new H();
+    A c=h.foo(a);
+
+    BenchmarkN.test(1, c);
+  }
+
+}
